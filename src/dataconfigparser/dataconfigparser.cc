b4896683c7232bc9c959a244b360694557858cf1
jyy <admin@xjrjyy.cn>
2022-04-09 19:53:24 +0800

    :bug: Fix DataConfigParser Bug

--- src/dataconfigparser/dataconfigparser.cc
+++ src/dataconfigparser/dataconfigparser.cc
@@ -15,16 +15,16 @@ bool DataConfigParser::Parse() {
     while (!config_file.eof()) {
         std::string line;
         std::getline(config_file, line);
-        std::size_t repeat = 1;
+        int repeat = 1;
         while (line.size() > 0 && line[0] == '@') {
             line = line.substr(1);
             if (line.find(' ') != line.npos) line.erase(line.find(' '));
-            repeat *= std::stoull(line);
+            repeat *= std::stoi(line);
             std::getline(config_file, line);
             if (config_file.eof()) break;
         }
         if (!line.empty() && (line.back() == '\r' || line.back() == '\n')) line.pop_back();
-        std::cout << repeat << ": " << line << std::endl;
+        // std::cout << repeat << ": " << line << std::endl;
         config_.insert(config_.end(), repeat, line);
     }
     return true;
